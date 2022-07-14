e407a4ad09a2c04465f27bca7e563a8750f22264
Beth <bethmelmtv@gmail.com>
2022-07-14 11:12:59 -0700

    added github users table and github login route for github controller

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE if EXISTS github_users CASCADE;
+
+CREATE TABLE github_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username TEXT NOT NULL,
+  email TEXT,
+  avatar TEXT
+);
