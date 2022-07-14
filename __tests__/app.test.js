c7596801788e8b03c2030788d9bd1cf0608dcda2
Beth <bethmelmtv@gmail.com>
2022-07-14 11:03:23 -0700

    worked on redirect user to github test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,22 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github'); 
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+  
+  it('it should redirect the user to github for oauth login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    
+    expect(res.header.location).toMatch(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user&redirect_`
+    );
+  })
+
+
   afterAll(() => {
     pool.end();
   });
