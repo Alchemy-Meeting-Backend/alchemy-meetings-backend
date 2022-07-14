e69934199e938b9bff2f3dca597695f0b81e9f0e
SusanBird <susanbird391@gmail.com>
2022-07-14 16:30:14 -0700

    wrote return list of users test

---
+++ __tests__/users.tests.js
@@ -0,0 +1,29 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Users = require('../lib/models/Users');
+
+jest.mock('../lib/services/github');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  it('/users should return a list of users', async () => {
+    const res = await request(app).get('/users');
+    const userData = Users.getAll();
+    const expected = (await userData).map((user) => {
+      return { username: user.username, email: user.email, cohort_id: user.cohort_id, role: user.role };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  
+  afterAll(() => {
+    pool.end();
+  });
+});
+
+
