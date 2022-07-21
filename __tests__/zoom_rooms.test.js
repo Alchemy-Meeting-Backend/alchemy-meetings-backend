2661a31ce2d1ff79579d322e822a3161574c050c
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-21 15:31:55 -0700

    all but one test passing, but need to add new mock user first

--- __tests__/zoom_rooms.test.js
+++ __tests__/zoom_rooms.test.js
@@ -71,7 +71,132 @@ describe('Zoom Room Tests', () => {
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
     const res = await agent.get('/api/v1/zoomrooms');
-    expect(res.body).toEqual([{ 'id': '1', 'room_name': 'Cobalt' }, { 'id': '2', 'room_name': 'Goodland' }, { 'id': '3', 'room_name': 'Copper' }]);
+    expect(res.body).toEqual([
+      {
+        'id': '1',
+        'room_name': 'Cobalt'
+      },
+      {
+        'id': '2',
+        'room_name': 'Goodland'
+      },
+      {
+        'id': '3',
+        'room_name': 'Copper'
+      },
+      {
+        'id': '4',
+        'room_name': 'Aluminum'
+      },
+      {
+        'id': '5',
+        'room_name': 'Beryllium'
+      },
+      {
+        'id': '6',
+        'room_name': 'Brass'
+      },
+      {
+        'id': '7',
+        'room_name': 'Bronze'
+      },
+      {
+        'id': '8',
+        'room_name': 'Chrome'
+      },
+      {
+        'id': '9',
+        'room_name': 'Gold'
+      },
+      {
+        'id': '10',
+        'room_name': 'Iron'
+      },
+      {
+        'id': '11',
+        'room_name': 'Lithium'
+      },
+      {
+        'id': '12',
+        'room_name': 'Mercury'
+      },
+      {
+        'id': '13',
+        'room_name': 'Nickel'
+      },
+      {
+        'id': '14',
+        'room_name': 'Osmium'
+      },
+      {
+        'id': '15',
+        'room_name': 'Oxygen'
+      },
+      {
+        'id': '16',
+        'room_name': 'Mythril'
+      },
+      {
+        'id': '17',
+        'room_name': 'Silver'
+      },
+      {
+        'id': '18',
+        'room_name': 'Titanium'
+      },
+      {
+        'id': '19',
+        'room_name': 'Vibranium'
+      },
+      {
+        'id': '20',
+        'room_name': 'Xenon'
+      },
+      {
+        'id': '21',
+        'room_name': 'Zinc'
+      },
+      {
+        'id': '22',
+        'room_name': 'Atrium'
+      },
+      {
+        'id': '23',
+        'room_name': 'Danis Room'
+      },
+      {
+        'id': '24',
+        'room_name': 'Lecture'
+      },
+      {
+        'id': '25',
+        'room_name': 'Career Development Room'
+      },
+      {
+        'id': '26',
+        'room_name': 'Community Room'
+      },
+      {
+        'id': '27',
+        'room_name': 'Open Work Room'
+      },
+      {
+        'id': '28',
+        'room_name': 'Quiet Room'
+      },
+      {
+        'id': '29',
+        'room_name': 'TA Room'
+      },
+      {
+        'id': '30',
+        'room_name': 'TA Room 2'
+      },
+      {
+        'id': '31',
+        'room_name': 'Alumni Room'
+      }
+    ]);
   });
   
 
@@ -91,7 +216,24 @@ describe('Zoom Room Tests', () => {
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
     const res = await agent.get('/api/v1/zoomrooms');
-    expect(res.body).toEqual([]);
+    expect(res.body).toEqual([
+      {
+        'id': '25',
+        'room_name': 'Career Development Room',     
+      },
+      {
+        'id': '26',
+        'room_name': 'Community Room',
+      },
+      {
+        'id': '27',
+        'room_name': 'Open Work Room',
+      },
+      {
+        'id': '31',
+        'room_name': 'Alumni Room',
+      },
+    ]);
   });
 
   afterAll(() => {
