dc2c601b570f99909c3076f540c96e47ba0b38d7
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-07-22 15:19:03 -0700

    all but one zoom_room test passing

--- __tests__/zoom_rooms.test.js
+++ __tests__/zoom_rooms.test.js
@@ -11,13 +11,21 @@ jest.mock('../lib/services/github', () => {
   };
 });
 
-describe.skip('Zoom Room Tests', () => {
+describe('Zoom Room Tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   const agent = request.agent(app);
   it('GET/ should display a list of zoomrooms associated with a users cohort id for authenticated users', async () => {
+    github.getGitHubProfile.mockImplementation(() => {
+      return {
+        login: 'someperson',
+        email: 'fakeusername@faux.net',
+        cohort_id: 9,
+        role: 'student'
+      };
+    });
 
     await agent
       .get('/api/v1/github/callback?code=42')
@@ -25,11 +33,7 @@ describe.skip('Zoom Room Tests', () => {
       
     const res = await agent.get('/api/v1/zoomrooms');
 
-    expect(res.body).toEqual([
-      { id: '1', room_name: 'Cobalt' },
-      { id: '2', room_name: 'Goodland' },
-      { id: '3', room_name: 'Copper' }
-    ]);
+    expect(res.body).toEqual([]);
 
   });
 
@@ -43,21 +47,21 @@ describe.skip('Zoom Room Tests', () => {
 
 
   it('GET/ should display no zoomrooms for pending users, who have a cohort id of 1', async () => {
-    const userResponse = await agent
+    github.getGitHubProfile.mockImplementation(() => {
+      return {
+        login: 'someperson',
+        email: 'fakeusername@faux.net',
+        cohort_id: 1,
+        role: 'student'
+      };
+    });
+
+    await agent
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
 
-    await agent
-      .put(`/api/v1/github/${userResponse.body.id}`)
-      .send({ cohort_id: 1 });
-
-    await agent
-      .delete('/api/v1/github/sessions');
-
-    await agent
-      .get('/api/v1/github/callback?code=42')
-      .redirects(1);
-    const res = await agent.get('/api/v1/zoomrooms');
+    const res = await agent
+      .get('/api/v1/zoomrooms');
     expect(res.body).toEqual([]);
   });
 
@@ -72,45 +76,209 @@ describe.skip('Zoom Room Tests', () => {
       };
     });
 
-
-    const userResponse = await agent
+    await agent
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
-    
-    // await agent
-    //   .put(`/api/v1/github/${userResponse.body.id}`)
-    //   .send({ cohort_id: 2 });
-
-    // await agent
-    //   .delete('/api/v1/github/sessions');
-
-
-    // await agent
-    //   .get('/api/v1/github/callback?code=42')
-    //   .redirects(1);
+ 
     const res = await agent.get('/api/v1/zoomrooms');
-    expect(res.body).toEqual([{ 'id': '1', 'room_name': 'Cobalt' }, { 'id': '2', 'room_name': 'Goodland' }, { 'id': '3', 'room_name': 'Copper' }]);
+    expect(res.body).toEqual([
+      {
+        'id': '1',
+        'meeting_id': '9040374817',
+        'room_name': 'Cobalt',
+      },
+      {
+        'id': '2',
+        'meeting_id': '9040374817',
+        'room_name': 'Goodland',
+      },
+      {
+        'id': '3',
+        'meeting_id': '9040374817',
+        'room_name': 'Copper',
+      },
+      {
+        'id': '4',
+        'meeting_id': '9040374817',
+        'room_name': 'Aluminum',
+      },
+      {
+        'id': '5',
+        'meeting_id': '9040374817',
+        'room_name': 'Beryllium',
+      },
+      {
+        'id': '6',
+        'meeting_id': '9040374817',
+        'room_name': 'Brass',
+      },
+      {
+        'id': '7',
+        'meeting_id': '9040374817',
+        'room_name': 'Bronze',
+      },
+      {
+        'id': '8',
+        'meeting_id': '9040374817',
+        'room_name': 'Chrome',
+      },
+      {
+        'id': '9',
+        'meeting_id': '9040374817',
+        'room_name': 'Gold',
+      },
+      {
+        'id': '10',
+        'meeting_id': '9040374817',
+        'room_name': 'Iron',
+      },
+      {
+        'id': '11',
+        'meeting_id': '9040374817',
+        'room_name': 'Lithium',
+      },
+      {
+        'id': '12',
+        'meeting_id': '9040374817',
+        'room_name': 'Mercury',
+      },
+      {
+        'id': '13',
+        'meeting_id': '9040374817',
+        'room_name': 'Nickel',
+      },
+      {
+        'id': '14',
+        'meeting_id': '9040374817',
+        'room_name': 'Osmium',
+      },
+      {
+        'id': '15',
+        'meeting_id': '9040374817',
+        'room_name': 'Oxygen',
+      },
+      {
+        'id': '16',
+        'meeting_id': '9040374817',
+        'room_name': 'Mythril',
+      },
+      {
+        'id': '17',
+        'meeting_id': '9040374817',
+        'room_name': 'Silver',
+      },
+      {
+        'id': '18',
+        'meeting_id': '9040374817',
+        'room_name': 'Titanium',
+      },
+      {
+        'id': '19',
+        'meeting_id': '9040374817',
+        'room_name': 'Vibranium',
+      },
+      {
+        'id': '20',
+        'meeting_id': '9040374817',
+        'room_name': 'Xenon',
+      },
+      {
+        'id': '21',
+        'meeting_id': '9040374817',
+        'room_name': 'Zinc',
+      },
+      {
+        'id': '22',
+        'meeting_id': '9040374817',
+        'room_name': 'Atrium',
+      },
+      {
+        'id': '23',
+        'meeting_id': '9040374817',
+        'room_name': 'Danis Room',
+      },
+      {
+        'id': '24',
+        'meeting_id': '9040374817',
+        'room_name': 'Lecture',
+      },
+      {
+        'id': '25',
+        'meeting_id': '9040374817',
+        'room_name': 'Career Development Room',
+      },
+      {
+        'id': '26',
+        'meeting_id': '9040374817',
+        'room_name': 'Community Room',
+      },
+      {
+        'id': '27',
+        'meeting_id': '9040374817',
+        'room_name': 'Open Work Room',
+      },
+      {
+        'id': '28',
+        'meeting_id': '9040374817',
+        'room_name': 'Quiet Room',
+      },
+      {
+        'id': '29',
+        'meeting_id': '9040374817',
+        'room_name': 'TA Room',
+      },
+      {
+        'id': '30',
+        'meeting_id': '9040374817',
+        'room_name': 'TA Room 2',
+      },
+      {
+        'id': '31',
+        'meeting_id': '9040374817',
+        'room_name': 'Alumni Room',
+      },
+    ]);
 
   });
   
 
   it('GET/ should display a list of all community zoomrooms for alumni, who have a cohort id of 3', async () => {
-    const userResponse = await agent
-      .get('/api/v1/github/callback?code=42')
-      .redirects(1);
-    
-    await agent
-      .put(`/api/v1/github/${userResponse.body.id}`)
-      .send({ cohort_id: 3 });
+    github.getGitHubProfile.mockImplementation(() => {
+      return {
+        login: 'someperson',
+        email: 'fakeusername@faux.net',
+        cohort_id: 3,
+        role: 'student'
+      };
+    });
 
     await agent
-      .delete('/api/v1/github/sessions');
-
-    await agent
       .get('/api/v1/github/callback?code=42')
       .redirects(1);
+
     const res = await agent.get('/api/v1/zoomrooms');
-    expect(res.body).toEqual([]);
+    expect(res.body).toEqual([
+      {
+        'id': '25',
+        'room_name': 'Career Development Room',
+        'meeting_id': '9040374817'
+      },
+      {
+        'id': '26',
+        'room_name': 'Community Room',
+        'meeting_id': '9040374817'
+      },
+      {
+        'id': '27',
+        'room_name': 'Open Work Room',
+        'meeting_id': '9040374817'
+      },
+      {
+        'id': '31',
+        'room_name': 'Alumni Room',
+        'meeting_id': '9040374817'
+      },
+    ]);
   });
 
   afterAll(() => {
